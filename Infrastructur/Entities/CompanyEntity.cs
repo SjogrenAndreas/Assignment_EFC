8247aae0ef05bd391d64ff093677e8fe911838fb
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-27 15:23:20 +0100

    updated

---
+++ Infrastructur/Entities/CompanyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructur.Entities
+{
+    internal class CompanyEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? CompanyName { get; set; }
+
+    }
+}
