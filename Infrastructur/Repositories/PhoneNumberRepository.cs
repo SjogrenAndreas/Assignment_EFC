4f690eb34021810aabde20f29aeb0280143055ae
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-27 22:26:20 +0100

    update

---
+++ Infrastructur/Repositories/PhoneNumberRepository.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructur.Contexts;
+using Infrastructur.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructur.Repositories
+{
+    internal class PhoneNumberRepository : Repo<PhoneNumberEntity>
+    {
+        public PhoneNumberRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructur/Repositories/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructur.Contexts;
+using Infrastructur.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructur.Repositories
+{
+    internal class RoleRepository : Repo<RoleEntity>
+    {
+        public RoleRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
