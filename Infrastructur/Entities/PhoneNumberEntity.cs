7adedf1641843a6e09cbc43343b350cea2beabc0
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-27 15:07:46 +0100

    update - changed gitignore

--- Infrastructur/Entities/PhoneNumberEntity.cs
+++ Infrastructur/Entities/PhoneNumberEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Infrastructur.Entities
 {
     internal class PhoneNumberEntity
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string WorkPhone { get; set; } = null!;
+
+        public string MobilePhone { get; set; } = null!;
+
     }
 }
