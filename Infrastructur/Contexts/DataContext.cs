b5eb9599e31f9950e1ac05852937b205e3236901
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-27 21:52:19 +0100

    updated

---
+++ Infrastructur/Contexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructur.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructur.Contexts
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressEntity> Addresses { get; set; }
+        public DbSet<CompanyEntity> Companies { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<PhoneNumberEntity> PhoneNumbers { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+    }
+
+}
