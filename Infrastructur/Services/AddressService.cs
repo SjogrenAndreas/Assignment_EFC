6443db09a0f0903080e4862934de759d1e1352b9
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-28 13:25:51 +0100

    updated

--- Infrastructur/Services/AddressService.cs
+++ Infrastructur/Services/AddressService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructur.Entities;
+using Infrastructur.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,49 @@ namespace Infrastructur.Services
 {
     internal class AddressService
     {
+        private readonly AddressRepository _addressRepository;
+
+        public AddressService(AddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public AddressEntity CreateAddress(string streetName, string postalCode, string city)
+        {
+            var addressEntity = _addressRepository.Get(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            addressEntity ??= _addressRepository.Create(new AddressEntity { StreetName = streetName, PostalCode = postalCode, City = city });
+
+            return addressEntity;
+        }
+
+        public AddressEntity GetAddress(string streetName, string postalCode, string city)
+        {
+            var addressEntity = _addressRepository.Get(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            return addressEntity;
+        }
+
+        public AddressEntity GetAddressById(int id)
+        {
+            var addressEntity = _addressRepository.Get(x => x.Id == id);
+            return addressEntity;
+        }
+
+        public IEnumerable<AddressEntity> GetAddresses()
+        {
+            var addresses = _addressRepository.GetAll();
+            return addresses;
+        }
+
+        public AddressEntity UpdateAddress(AddressEntity addressEntity)
+        {
+            var updatedAddressEntity = _addressRepository.Update(x => x.Id == addressEntity.Id, addressEntity);
+            return updatedAddressEntity;
+        }
+
+
+        public void DeleteAddress(int id)
+        {
+            _addressRepository.Delete(x => x.Id == id);
+        }
     }
 }
