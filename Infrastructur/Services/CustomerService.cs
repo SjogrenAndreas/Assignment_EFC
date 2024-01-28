f23c8804a71e3262fe9b9cb3d1432aba9df216c6
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-28 19:10:07 +0100

    update

--- Infrastructur/Services/CustomerService.cs
+++ Infrastructur/Services/CustomerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructur.Entities;
+using Infrastructur.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,49 @@ namespace Infrastructur.Services
 {
     internal class CustomerService
     {
+        private readonly CustomerRepository _customerRepository;
 
+        public CustomerService(CustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public CustomerEntity CreateCustomer(string streetName, string postalCode, string city)
+        {
+            var customerEntity = _customerRepository.Get(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            customerEntity ??= _customerRepository.Create(new CustomerEntity { StreetName = streetName, PostalCode = postalCode, City = city });
+
+            return customerEntity;
+        }
+
+        public CustomerEntity GetCustomer(string streetName, string postalCode, string city)
+        {
+            var customerEntity = _customerRepository.Get(x => x.StreetName == streetName && x.PostalCode == postalCode && x.City == city);
+            return customerEntity;
+        }
+
+        public CustomerEntity GetCustomerById(int id)
+        {
+            var customerEntity = _customerRepository.Get(x => x.Id == id);
+            return customerEntity;
+        }
+
+        public IEnumerable<CustomerEntity> GetCustomeres()
+        {
+            var customeres = _customerRepository.GetAll();
+            return customeres;
+        }
+
+        public CustomerEntity UpdateCustomer(CustomerEntity customerEntity)
+        {
+            var updatedCustomerEntity = _customerRepository.Update(x => x.Id == customerEntity.Id, customerEntity);
+            return updatedCustomerEntity;
+        }
+
+
+        public void DeleteCustomer(int id)
+        {
+            _customerRepository.Delete(x => x.Id == id);
+        }
     }
 }
---
+++ Infrastructur/Services/PhoneNumberService.cs
@@ -0,0 +1,58 @@
+﻿using Infrastructur.Entities;
+using Infrastructur.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructur.Services
+{
+    internal class PhoneNumberService
+    {
+        private readonly PhoneNumberRepository _phoneNumberRepository;
+
+        public PhoneNumberService(PhoneNumberRepository phoneNumberRepository)
+        {
+            _phoneNumberRepository = phoneNumberRepository;
+        }
+
+        public PhoneNumberEntity CreatePhoneNumber(string workPhone, string postalCode)
+        {
+            var phoneNumberEntity = _phoneNumberRepository.Get(x => x.WorkPhone == workPhone && x.MobilePhone == postalCode);
+            phoneNumberEntity ??= _phoneNumberRepository.Create(new PhoneNumberEntity { WorkPhone = workPhone, MobilePhone = postalCode });
+
+            return phoneNumberEntity;
+        }
+
+        public PhoneNumberEntity GetPhoneNumber(string workPhone, string postalCode)
+        {
+            var phoneNumberEntity = _phoneNumberRepository.Get(x => x.WorkPhone == workPhone && x.MobilePhone == postalCode );
+            return phoneNumberEntity;
+        }
+
+        public PhoneNumberEntity GetPhoneNumberById(int id)
+        {
+            var phoneNumberEntity = _phoneNumberRepository.Get(x => x.Id == id);
+            return phoneNumberEntity;
+        }
+
+        public IEnumerable<PhoneNumberEntity> GetPhoneNumberes()
+        {
+            var phoneNumberes = _phoneNumberRepository.GetAll();
+            return phoneNumberes;
+        }
+
+        public PhoneNumberEntity UpdatePhoneNumber(PhoneNumberEntity phoneNumberEntity)
+        {
+            var updatedPhoneNumberEntity = _phoneNumberRepository.Update(x => x.Id == phoneNumberEntity.Id, phoneNumberEntity);
+            return updatedPhoneNumberEntity;
+        }
+
+
+        public void DeletePhoneNumber(int id)
+        {
+            _phoneNumberRepository.Delete(x => x.Id == id);
+        }
+    }
+}
