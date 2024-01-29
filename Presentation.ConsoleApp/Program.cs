93838b7380d8e02ca793da5c018c296743e474e0
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-01-29 19:26:43 +0100

    updated

--- Presentation.ConsoleApp/Program.cs
+++ Presentation.ConsoleApp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using Infrastructur.Contexts;
+using Infrastructur.Repositories;
+using Infrastructur.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Presentation.ConsoleApp;
 
 var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 {
     services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Skola\Assignment_EFC\Infrastructur\Data\local_database.mdf;Integrated Security=True"));
     
+    services.AddScoped<AddressRepository>();
+    services.AddScoped<CompanyRepository>();
+    services.AddScoped<CustomerRepository>();
+    services.AddScoped<PhoneNumberRepository>();
+    services.AddScoped<RoleRepository>();
+
+    services.AddScoped<AddressService>();
+    services.AddScoped<CompanyService>();  
+    services.AddScoped<CustomerService>(); 
+    services.AddScoped<PhoneNumberService>();  
+    services.AddScoped<RoleService>();
+
+    services.AddSingleton<ConsoleUI>();
     
 
 }).Build();
+
+var consoleUI = builder.Services.GetRequiredService<ConsoleUI>();  
